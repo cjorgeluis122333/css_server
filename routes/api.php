1628238a60071779ddd2d632d048ddcf467c3d65
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-02-18 18:02:10 -0500

    Catch errors 401

--- routes/api.php
+++ routes/api.php
@@ -1,33 +1,27 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FamilyController;
 use App\Http\Controllers\PartnerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::get('/test', function () {
-    return "You are connected with the server";
-});
-
 // --- Public route ---
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-
     // Rutas automáticas para Socios (Titulares)
-    Route::apiResource('partners', PartnerController::class);
-
-// Rutas automáticas para Familiares
-    Route::apiResource('family', \App\Http\Controllers\FamilyController::class);
+    Route::apiResource('/partners', PartnerController::class);
+    // Rutas automáticas para Familiares
+    Route::apiResource('/family', FamilyController::class);
 });
 
 
