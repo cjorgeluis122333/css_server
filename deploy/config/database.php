cd87eb0e37ea9fc7ad7576d1b3bbce03e57691ec
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-07-27 10:58:12 -0400

    feat: Allow traffic with the hosting

--- deploy/config/database.php
+++ deploy/config/database.php
@@ -58,11 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            'options' => extension_loaded('pdo_mysql') ? [
+                PDO::MYSQL_ATTR_SSL_CA => storage_path('cacert.pem'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-//                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            ] : [],
         ],
 
         'mariadb' => [
