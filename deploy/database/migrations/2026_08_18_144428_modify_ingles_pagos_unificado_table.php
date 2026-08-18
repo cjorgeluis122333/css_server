1d066cf2cd85593efd9b0f8844775fd5a4090df2
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-08-18 11:46:07 -0400

    fix: Miss an import in english migration

--- deploy/database/migrations/2026_08_18_144428_modify_ingles_pagos_unificado_table.php
+++ deploy/database/migrations/2026_08_18_144428_modify_ingles_pagos_unificado_table.php
@@ -1,42 +1,41 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
-    public function up(): void
+    return new class extends Migration
     {
-        DB::statement(
-            'ALTER TABLE 0cc_ingles_pagos_unificado DROP PRIMARY KEY'
-        );
+        public function up(): void
+        {
+            DB::statement(
+                'ALTER TABLE 0cc_ingles_pagos_unificado DROP PRIMARY KEY'
+            );
 
-        DB::statement(
-            'ALTER TABLE 0cc_ingles_pagos_unificado ADD COLUMN id INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST'
-        );
+            DB::statement(
+                'ALTER TABLE 0cc_ingles_pagos_unificado ADD COLUMN id INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST'
+            );
 
-        DB::statement(
-            'ALTER TABLE 0cc_ingles_pagos_unificado DROP COLUMN ind'
-        );
-    }
+            DB::statement(
+                'ALTER TABLE 0cc_ingles_pagos_unificado DROP COLUMN ind'
+            );
+        }
 
-    public function down(): void
-    {
-        DB::statement(
-            'ALTER TABLE 0cc_ingles_pagos_unificado ADD COLUMN ind INT UNSIGNED NOT NULL'
-        );
+        public function down(): void
+        {
+            DB::statement(
+                'ALTER TABLE 0cc_ingles_pagos_unificado ADD COLUMN ind INT UNSIGNED NOT NULL'
+            );
 
-        DB::statement(
-            'ALTER TABLE 0cc_ingles_pagos_unificado DROP PRIMARY KEY'
-        );
+            DB::statement(
+                'ALTER TABLE 0cc_ingles_pagos_unificado DROP PRIMARY KEY'
+            );
 
-        DB::statement(
-            'ALTER TABLE 0cc_ingles_pagos_unificado DROP COLUMN id'
-        );
+            DB::statement(
+                'ALTER TABLE 0cc_ingles_pagos_unificado DROP COLUMN id'
+            );
 
-        DB::statement(
-            'ALTER TABLE 0cc_ingles_pagos_unificado ADD PRIMARY KEY (ano_tabla, ind)'
-        );
-    }
-};
+            DB::statement(
+                'ALTER TABLE 0cc_ingles_pagos_unificado ADD PRIMARY KEY (ano_tabla, ind)'
+            );
+        }
+    };
